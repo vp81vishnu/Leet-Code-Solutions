c7bcf14b699f94e84c5c421ca6dda2bc70480f44
vp81vishnu <99182049+vp81vishnu@users.noreply.github.com>
2024-04-16 10:02:20 +0530

    Time: 21 ms (9.63%), Space: 9.7 MB (95.49%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+       int arr[256]={0};
+        int maxi;
+        string result;
+       for(int i=0;i<s.length();i++){
+           arr[s[i]]++;
+       }
+       maxi=findmax(arr); 
+        while(maxi>0){
+            for(int i=0;i<256;i++){
+                if(arr[i]==maxi){
+                  result.append(arr[i],i);
+                }
+            }
+            maxi--;
+        }
+        
+        return result;
+    }
+    private:
+    int findmax(int arr[]){
+    int maximum=0;
+    for(int i=0;i<256;i++){
+        maximum=max(maximum,arr[i]);
+    }
+    return maximum;
+}
+};
+ 
