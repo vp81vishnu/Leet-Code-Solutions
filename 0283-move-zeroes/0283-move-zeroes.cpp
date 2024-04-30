9c63e8c0e86a5c1b9afce834a6a5559e4cbdb169
vp81vishnu <99182049+vp81vishnu@users.noreply.github.com>
2024-04-30 15:17:18 +0530

    Time: 15 ms (68.47%), Space: 21.7 MB (37.54%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& arr) {
+        int count=0;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]!=0){
+                arr[count++]=arr[i];
+            }
+        }
+        for(int i=count;i<arr.size();i++){
+            arr[count++]=0;
+        }
+        
+    }
+};
