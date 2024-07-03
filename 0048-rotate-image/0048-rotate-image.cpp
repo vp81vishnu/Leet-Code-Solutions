6bfca02c2618cd530f6d6d3b361ae5099f545d1d
vp81vishnu <99182049+vp81vishnu@users.noreply.github.com>
2024-07-03 09:22:44 +0530

    Time: 4 ms (38.37%), Space: 8.7 MB (61.1%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -4,13 +4,13 @@ public:
         int row=matrix.size();
         int col=matrix[0].size();
         for(int i=0;i<row;i++){
-            for(int j=i;j<col;j++){
+            for(int j=i+1;j<col;j++){
                 swap(matrix[i][j],matrix[j][i]);
             }
         }
         for(int i=0;i<row;i++){
              reverse(matrix[i].begin(),matrix[i].end());
-
-        }
+             }
+       
     }
 };
