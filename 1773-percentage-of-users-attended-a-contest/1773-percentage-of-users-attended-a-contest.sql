e9d3da927840571e741b4077a18c9511d7a98830
Vishnu Prasath <99182049+vp81vishnu@users.noreply.github.com>
2024-08-16 21:49:19 +0530

    Time: 1002 ms (47.09%), Space: 0B (100%) - LeetHub

---
+++ 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,9 @@
+SELECT 
+    r.contest_id,
+    ROUND(COUNT(DISTINCT r.user_id) * 100.0 / (SELECT COUNT(DISTINCT user_id) FROM Users), 2) AS percentage
+FROM 
+    Register r
+GROUP BY 
+    r.contest_id
+ORDER BY 
+    percentage DESC, r.contest_id ASC;
