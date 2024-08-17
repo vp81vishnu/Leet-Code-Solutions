65280a665229b1a514d87f450fe2aaab5601548f
Vishnu Prasath <99182049+vp81vishnu@users.noreply.github.com>
2024-08-17 09:43:19 +0530

    Time: 1000 ms (7.94%), Space: 0B (100%) - LeetHub

--- 2087-confirmation-rate/2087-confirmation-rate.sql
+++ 2087-confirmation-rate/2087-confirmation-rate.sql
@@ -1,5 +1,5 @@
-SELECT s.user_id,
-       ROUND(AVG(IF(c.action = 'confirmed', 1, 0)), 2) AS confirmation_rate
-FROM Signups s
-LEFT JOIN Confirmations c ON s.user_id = c.user_id
-GROUP BY s.user_id;
+select s.user_id,round(avg(if(c.action="confirmed",1,0)),2) as confirmation_rate
+from Signups s
+left join Confirmations c
+on s.user_id=c.user_id 
+group by s.user_id
