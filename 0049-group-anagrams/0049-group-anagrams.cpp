a58c61052ce7303595a22eda5bb95d9788eb06a7
Vishnu Prasath <99182049+vp81vishnu@users.noreply.github.com>
2024-08-20 09:56:29 +0530

    Time: 2585 ms (5.13%), Space: 503.2 MB (5.15%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,44 +1,48 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
 class Solution {
-public:
-    bool is_anagram(const string& a, const string& b) {
-        if (a.size() != b.size()) return false;
+    bool check(const string &str1, const string &str2) {
+        if (str1.size() != str2.size()) return false;
 
         vector<int> count(26, 0);
 
-        for (char c : a) {
+        for (char c : str1) {
             count[c - 'a']++;
         }
-
-        for (char c : b) {
+        for (char c : str2) {
             count[c - 'a']--;
         }
-
-        for (int i = 0; i < 26; i++) {
-            if (count[i] != 0) return false;
+        for (int i : count) {
+            if (i != 0) return false;
         }
-
         return true;
     }
 
+public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> res;
         vector<bool> visited(strs.size(), false);
 
-        for (int i = 0; i < strs.size(); i++) {
+        for (int i = 0; i < strs.size(); ++i) {
             if (visited[i]) continue;
-            vector<string> temp;
-            temp.push_back(strs[i]);
+
+            vector<string> ans;
+            ans.push_back(strs[i]); // Use strs[i] directly
             visited[i] = true;
 
-            for (int j = i + 1; j < strs.size(); j++) {
-                if (!visited[j] && is_anagram(strs[i], strs[j])) {
-                    temp.push_back(strs[j]);
+            for (int j = i + 1; j < strs.size(); ++j) {
+                if (visited[j]) continue;
+
+                if (check(strs[j], strs[i])) { // Compare strs[j] with strs[i]
                     visited[j] = true;
+                    ans.push_back(strs[j]);
                 }
             }
-            res.push_back(temp);
+            res.push_back(ans);
         }
-
         return res;
     }
 };
