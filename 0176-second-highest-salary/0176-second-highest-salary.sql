e625e53963696afaa5c192c297099632b598ac6b
Vishnu Prasath <99182049+vp81vishnu@users.noreply.github.com>
2024-08-30 22:06:46 +0530

    Time: 241 ms (65.65%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,11 +1,5 @@
 SELECT 
-    CASE 
-        WHEN (SELECT COUNT(DISTINCT salary) FROM employee) > 1 
-        THEN (
-            SELECT DISTINCT salary 
-            FROM employee 
-            ORDER BY salary DESC 
-            LIMIT 1 OFFSET 1
-        )
-        ELSE NULL
-    END AS SecondHighestSalary;
+    (SELECT DISTINCT salary 
+     FROM employee 
+     ORDER BY salary DESC 
+     LIMIT 1 OFFSET 1) AS SecondHighestSalary;
